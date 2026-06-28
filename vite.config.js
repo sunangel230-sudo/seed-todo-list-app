10901e50917cf9c670159600b25094362be9e910
sunangel230 <sunangel230@email.com>
2026-06-27 19:02:05 -0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { cloudflare } from "@cloudflare/vite-plugin"
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cloudflare()],
+
+  server: {
+    port: 5173
+  },
+
   base: process.env.GITHUB_ACTIONS ? '/seed-todo-list-app/' : '/'
 })
