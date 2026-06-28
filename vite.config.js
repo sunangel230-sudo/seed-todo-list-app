d5925d3e752c6c26d9d03970050d91c0e61f4303
sunangel230 <sunangel230@email.com>
2026-06-27 18:47:57 -0700

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
-
 import vue from '@vitejs/plugin-vue'
 
-
 export default defineConfig({
-
-  plugins:[
-    vue()
-  ],
-
-  server:{
-    port:5173
-  }
-
+  plugins: [vue()],
+  base: process.env.GITHUB_ACTIONS ? '/seed-todo-list-app/' : '/'
 })
